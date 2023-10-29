2625ee17b6eaaa9cea586c97951e79e193a63b6d
Alejandro <alejandroquiceno.aq@gmail.com>
2023-10-29 13:51:56 -0500

    version 1.0.3.1

---
+++ pages/pinch_activity_page.py
@@ -0,0 +1,11 @@
+from appium.webdriver.common.appiumby import AppiumBy
+from utils.launch_app import KWALauncher
+from utils.actions_gesture import ActionGestures
+
+app_launcher = KWALauncher()
+action_gestures = ActionGestures()
+
+
+class PinchActivityPage:
+    def __init__(self):
+        pass
--- tests/test_homefeed.py
+++ tests/test_homefeed.py
@@ -3,27 +3,22 @@ from pages.homefeed_page import HomefeedPage
 home_buttons = HomefeedPage()
 
 
-def test_enter_value_button():
+def test_send_values_to_kwa_demo():
     home_buttons.choose_enter_value()
     home_buttons.go_back_button()
 
 
-def test_contact_button():
+def test_user_submits_contact_information():
     home_buttons.choose_contact_us_from()
     home_buttons.go_back_button()
 
 
-def test_scroll_view():
-    home_buttons.choose_scrollview()
-    home_buttons.go_back_button()
-
-
-def test_tab_activity():
+def test_move_between_home_sports_movie_tabs():
     home_buttons.choose_tab_activity()
     home_buttons.go_back_button()
 
 
-def test_zoom():
+def test_change_of_logo_display():
     home_buttons.choose_zoom()
 
 
@@ -32,39 +27,44 @@ def test_login():
     home_buttons.go_back_button()
 
 
-def test_longclick():
+def test_get_password_of_the_entered_email():
     home_buttons.choose_long_click()
 
 
-def test_time():
+def test_enter_the_time_activity():
     home_buttons.choose_time()
     home_buttons.go_back()
 
 
-def test_date():
+def test_enter_the_date_activity():
     home_buttons.choose_date()
     home_buttons.go_back()
 
 
-def test_hybrid():
+def test_connectivity_between_website_and_app():
     home_buttons.choose_hybrid()
     home_buttons.go_back_button()
     
 
-def test_pinch_in_put():
+def test_increase_or_decrease_the_user_logo():
     home_buttons.choose_pinch_in_out()
     home_buttons.go_back()
 
 
-def test_drag_and_drop():
+def test_change_position_of_items_by_user_categories():
     home_buttons.choose_draganddrop()
     home_buttons.go_back()
     
 
-def test_auto_suggestion():
+def test_enter_auto_suggestion():
     home_buttons.choose_auto_suggestion()
     home_buttons.go_back()
 
 
-def test_crash():
+def test_crash_simulation():
     home_buttons.choose_crash()
+
+
+def test_scroll_view():
+    home_buttons.choose_scrollview()
+    home_buttons.go_back_button()
