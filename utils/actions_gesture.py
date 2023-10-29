7b4d61e7768cf3125b4345dbadf0d4e678f69d35
Alejandro <alejandroquiceno.aq@gmail.com>
2023-10-29 12:59:29 -0500

    Version 1.0.3

--- utils/actions_gesture.py
+++ utils/actions_gesture.py
@@ -32,12 +32,12 @@ class ActionGestures(ResolutionDevice):
         element = WebDriverWait(self.driver, timeout).until(ec.element_to_be_clickable(value))
         element.click()
 
-    def scroll_to_element_click(self, by, value, timeout=30):
-        element_scroll = WebDriverWait(self.driver, timeout).until(ec.presence_of_element_located((by, value)))
+    def scroll_to_element_click(self, value, timeout=30):
+        element_scroll = WebDriverWait(self.driver, timeout).until(ec.presence_of_element_located(value))
         element_scroll.click()
 
-    def scroll_to_element(self, by, value, timeout=30):
-        WebDriverWait(self.driver, timeout).until(ec.presence_of_element_located((by, value)))
+    def scroll_to_element(self, value, timeout=30):
+        WebDriverWait(self.driver, timeout).until(ec.presence_of_element_located(value))
 
     def click_back_button(self):
         self.wait_and_click(self.BUTTON_NAVIGATE_UP)
@@ -45,26 +45,26 @@ class ActionGestures(ResolutionDevice):
     def back_driver(self):
         self.driver.back()
 
-    def long_click(self, by, value, timeout=30):
-        element = WebDriverWait(self.driver, timeout).until(ec.element_to_be_clickable((by, value)))
+    def long_click(self, value, timeout=30):
+        element = WebDriverWait(self.driver, timeout).until(ec.element_to_be_clickable(value))
         action = ActionChains(self.driver)
         action.click_and_hold(element).perform()
         # Remember:
         # ActionChains(driver) for Web Applications (Hybrid).
         # TouchActions(driver) for native Applications (iOS & Android).
 
-    def tap_gesture(self, by, value, timeout, x, y, time):
-        element = WebDriverWait(self.driver, timeout).until(ec.presence_of_element_located((by, value)))
+    def tap_gesture(self, value, timeout, x, y, time):
+        element = WebDriverWait(self.driver, timeout).until(ec.presence_of_element_located(value))
         actions = TouchAction(self.driver)
         actions.tap(element, x, y, time)
         # (element, CorX,Cor Y, mil seg)
         actions.perform()
         # perform() mandatory for execute the TouchAction(driver)
 
-    def drag_drop(self, by, value, timeout=30):
+    def drag_drop(self, value, timeout=30):
         # Define element positions
-        element_a = WebDriverWait(self.driver, timeout).until(ec.element_to_be_clickable((by, value)))
-        element_b = WebDriverWait(self.driver, timeout).until(ec.visibility_of_element_located((by, value)))
+        element_a = WebDriverWait(self.driver, timeout).until(ec.element_to_be_clickable(value))
+        element_b = WebDriverWait(self.driver, timeout).until(ec.visibility_of_element_located(value))
         # TouchActions(driver) library
         actions = TouchAction(self.driver)
         # Press Element. Then, move to element b position.
